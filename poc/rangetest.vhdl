ac56bba4d652d1535c806c7b09364f177b3dff8c
Brian Padalino <bpadalino@gmail.com>
2023-03-01 09:26:55 -0500

    Updates.

--- poc/rangetest.vhdl
+++ poc/rangetest.vhdl
@@ -5,6 +5,7 @@ end entity ;
 
 architecture arch of test is
 
+
     function generic_overlap
       generic (
         type array_t is array(natural range <>) of type is private ;
@@ -12,40 +13,21 @@ architecture arch of test is
         l, r : array_t ;
       ) return natural'range'record
     is
-        constant lrange : natural'range'record := l'range'value ;
-        constant rrange : natural'range'record := r'range'value ;
-        variable orange : natural'range'record := l'range'value ;
+        constant lrange : natural'range'record := l'range'value when l'ascending = true else l'reverse_range'value ;
+        constant rrange : natural'range'record := r'range'value when r'ascending = true else r'reverse_range'value ;
+        variable orange : natural'range'record := lrange ;
     begin
-        if lrange.direction /= rrange.direction then
-            -- Choosing to be ascending
-            orange.direction := ascending ;
-            if lrange.direction = ascending then
-                -- L: low to high
-                -- R: high downto low
-                orange.left  := maximum(lrange.left, rrange.right) ;
-                orange.right := minimum(lrange.right, rrange.left) ;
-            else
-                -- L: high downto low
-                -- R: low to high
-                orange.left  := maximum(lrange.right, rrange.left) ;
-                orange.right := minimum(lrange.left, rrange.right) ;
-            end if ;
-        else
-            if lrange.direction = ascending then
-                -- low to high
-                orange.left  := maximum(lrange.left, rrange.left) ;
-                orange.right := minimum(lrange.right, rrange.right) ;
-            else
-                -- high downto low
-                orange.left  := minimum(lrange.left, rrange.left) ;
-                orange.right := maximum(lrange.right, rrange.right) ;
-            end if ;
-        end if ;
+        -- All directions are ascending so we just need to take
+        -- the minimum of the maximum of the left and the minimum
+        -- of the right, and that should give us our overlap
+        orange.left := maximum(lrange.left, rrange.left) ;
+        orange.right := minimum(lrange.right, rrange.right) ;
         return orange ;
     end function ;
 
 
 /*
+    -- Riviera-PRO can't figure out how to map the array types
     function generic_overlap
       generic (
         type array_t is array(type is (<>)) of type is private ;
@@ -55,39 +37,21 @@ architecture arch of test is
         l, r : array_t ;
       ) return array_t'index'range'record
     is
-        constant lrange : array_t'index'range'record := l'range'value ;
-        constant rrange : array_t'index'range'record := r'range'value ;
-        variable orange : array_t'index'range'record := l'range'value ;
+        constant lrange : array_t'index'range'record := l'range'value when l'ascending = true else l'reverse_range'value ;
+        constant rrange : array_t'index'range'record := r'range'value when r'ascending = true else r'reverse_range'value ;
+        variable orange : array_t'index'range'record := lrange ;
     begin
-        if lrange.direction /= rrange.direction then
-            -- Choosing to be ascending
-            orange.direction := ascending ;
-            if lrange.direction = ascending then
-                -- L: low to high
-                -- R: high downto low
-                orange.left := maximum(lrange.left, rrange.right) ;
-                orange.right := minimum(lrange.right, rrange.left) ;
-            else
-                -- L: high downto low
-                -- R: low to high
-                orange.left := maximum(lrange.right, rrange.left) ;
-                orange.right := minimum(lrange.left, rrange.right) ;
-            end if ;
-        else
-            if lrange.direction = ascending then
-                -- low to high
-                orange.left := maximum(lrange.left, rrange.left) ;
-                orange.right := minimum(lrange.right, rrange.right) ;
-            else
-                -- high downto low
-                orange.left := minimum(lrange.left, rrange.left) ;
-                orange.right := maximum(lrange.right, rrange.right) ;
-            end if ;
-        end if ;
+        -- All directions are ascending so we just need to take
+        -- the minimum of the maximum of the left and the minimum
+        -- of the right, and that should give us our overlap
+        orange.left := maximum(lrange.left, rrange.left) ;
+        orange.right := minimum(lrange.right, rrange.right) ;
         return orange ;
     end function ;
 */
 
+    -- Riviera-PRO doesn't like overloaded generic and instantiated functions
+    -- function overlap is new overlap generic map( array_t => bit_vector ) ;
     function overlap is new generic_overlap generic map( array_t => bit_vector ) ;
 
     signal a : bit_vector(7 downto 0) ;
@@ -100,60 +64,63 @@ architecture arch of test is
 
     function to_string(x : natural'range'record) return string is
     begin
-        return "( left: " & to_string(x.left) & ", right: " & to_string(x.right) & ", direction: " & to_string(x.direction) & ")" ;
+        return "(left: " & to_string(x.left) & ", right: " & to_string(x.right) & ", direction: " & to_string(x.direction) & ")" ;
     end function ;
 
 begin
 
     tb : process
+        variable l : std.textio.line ;
     begin
         -- A's
-        report to_string(overlap(a, a)) ;
-        report to_string(overlap(a, b)) ;
-        report to_string(overlap(a, c)) ;
-        report to_string(overlap(a, d)) ;
-        report to_string(overlap(a, e)) ;
-        report to_string(overlap(a, f)) ;
+        std.textio.write(l, to_string(overlap(a, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(a, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(a, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(a, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(a, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(a, f)) & LF) ;
 
         -- B's
-        report to_string(overlap(b, a)) ;
-        report to_string(overlap(b, b)) ;
-        report to_string(overlap(b, c)) ;
-        report to_string(overlap(b, d)) ;
-        report to_string(overlap(b, e)) ;
-        report to_string(overlap(b, f)) ;
+        std.textio.write(l, to_string(overlap(b, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(b, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(b, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(b, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(b, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(b, f)) & LF) ;
 
         -- C's
-        report to_string(overlap(c, a)) ;
-        report to_string(overlap(c, b)) ;
-        report to_string(overlap(c, c)) ;
-        report to_string(overlap(c, d)) ;
-        report to_string(overlap(c, e)) ;
-        report to_string(overlap(c, f)) ;
+        std.textio.write(l, to_string(overlap(c, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(c, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(c, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(c, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(c, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(c, f)) & LF) ;
 
         -- D's
-        report to_string(overlap(d, a)) ;
-        report to_string(overlap(d, b)) ;
-        report to_string(overlap(d, c)) ;
-        report to_string(overlap(d, d)) ;
-        report to_string(overlap(d, e)) ;
-        report to_string(overlap(d, f)) ;
+        std.textio.write(l, to_string(overlap(d, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(d, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(d, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(d, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(d, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(d, f)) & LF) ;
 
         -- E's
-        report to_string(overlap(e, a)) ;
-        report to_string(overlap(e, b)) ;
-        report to_string(overlap(e, c)) ;
-        report to_string(overlap(e, d)) ;
-        report to_string(overlap(e, e)) ;
-        report to_string(overlap(e, f)) ;
+        std.textio.write(l, to_string(overlap(e, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(e, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(e, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(e, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(e, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(e, f)) & LF) ;
 
         -- F's
-        report to_string(overlap(f, a)) ;
-        report to_string(overlap(f, b)) ;
-        report to_string(overlap(f, c)) ;
-        report to_string(overlap(f, d)) ;
-        report to_string(overlap(f, e)) ;
-        report to_string(overlap(f, f)) ;
+        std.textio.write(l, to_string(overlap(f, a)) & LF) ;
+        std.textio.write(l, to_string(overlap(f, b)) & LF) ;
+        std.textio.write(l, to_string(overlap(f, c)) & LF) ;
+        std.textio.write(l, to_string(overlap(f, d)) & LF) ;
+        std.textio.write(l, to_string(overlap(f, e)) & LF) ;
+        std.textio.write(l, to_string(overlap(f, f)) & LF) ;
+
+        std.textio.writeline(std.textio.output, l) ;
 
         -- Done
         std.env.stop ;
--- util/util.vhdl
+++ util/util.vhdl
@@ -1,25 +1,226 @@
+-- Balanced Tree
+package btree is
+
+end package ;
+
+-- Ordered List
+package list is
+    -- type could be value_mirror to hold a bunch of different records?
+    -- generic package or generic protected type?
+    -- generic protected type makes more sense
+    type list is protected generic(type t) ;
+        procedure prepend(x : t) ;
+        procedure append(x : t) ;
+        procedure insert(x : t ; index : natural) ;
+        procedure clear(r : natural'range'record) ;
+        impure function size return natural ;
+        impure function get(index : natural) return rv_t of t ;
+        impure function iterate(from : natural := 0) return natural'range'record ;
+    end protected ;
+end package ;
+
+package body list is
+
+    type list is protected body
+        variable lsize : natural ;
+
+        procedure prepend(x : t) is
+        begin
+        end procedure ;
+
+        procedure append(x : t) is
+        begin
+        end procedure ;
+
+        procedure insert(x : t ; index : natural) is
+        begin
+
+        end procedure ;
+
+        procedure clear(r : natural'range'record) is
+        begin
+
+        end procedure ;
+
+        impure function size return natural is
+        begin
+            return lsize ;
+        end function ;
+
+        impure function get(index : natural) return rv_t of t is
+            variable rv : rv_t ;
+        begin
+            return rv ;
+        end function ;
+
+        impure function iterate(from : natural := 0) return natural'range'record is
+            subtype rv is natural range from to lsize-1 ;
+        begin
+            return rv'range'record ;
+        end function ;
+    end protected body ;
+
+end package body ;
+
+-- Unordered Dictionary
+package dict is
+    type dict is protected
+      generic(
+        type keys_t is array(natural range <>) of type is private ;
+        type value_t ;
+        function "<"(l, r : keys_t'element) return boolean is <>
+      ) ;
+        procedure add(k : keys_t'element ; v : value_t) ;
+        impure function keys return keys_t ;
+        impure function get(key : keys_t'element) return value_t ;
+        impure function exists(key : keys_t'element) return boolean ;
+        impure function size return natural ;
+    end protected ;
+end package ;
+
+package body dict is
+
+    type dict is protected body
+        variable lsize : natural ;
+        procedure add(k : keys_t'element ; v : value_t) is
+        begin
+
+        end procedure ;
+
+        impure function keys return keys_t is
+            variable rv : keys_t(0 to lsize-1) ;
+        begin
+            return rv ;
+        end function ;
+
+        impure function get(key : keys_t'element) return value_t is
+            variable rv : value_t ;
+        begin
+            return rv ;
+        end function ;
+
+        impure function exists(key : keys_t'element) return boolean is
+        begin
+            return false ;
+        end function ;
+
+        impure function size return natural is
+        begin
+            return lsize ;
+        end function ;
+    end protected body ;
+
+end package body ;
+
+package memory is
+
+    type memory is protected
+        impure function read(r : natural'range'record) return rv_t of bit_vector ;
+        procedure write(r : natural'range'record ; v : bit_vector) ;
+        procedure randomize(r : natural'range'record) ;
+        procedure clear(r : natural'range'record) ;
+    end protected ;
+
+end package ;
+
+package body memory is
+
+    -- Create a btree of flat memory with ranges?
+    type memory is protected body
+        impure function read(r : natural'range'record) return rv_t of bit_vector is
+            variable rv : rv_t ;
+        begin
+            return rv ;
+        end function ;
+
+        procedure write(r : natural'range'record ; v : bit_vector) is
+        begin
+
+        end procedure ;
+
+        procedure randomize(r : natural'range'record) is
+        begin
+
+        end procedure ;
+
+        procedure clear(r : natural'range'record) is
+        begin
+
+        end procedure ;
+    end protected body ;
+
+end package body ;
+
 use std.reflection.all ;
 
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
 package util is
+
+    -- binary file interaction
 
     package sim is
         impure function to_string(variable value : value_mirror) return string ;
 
+
         -- Good functions to have for any data type
         --function pack(x : sometype) return rv_t of std_ulogic_vector ;
         --function unpack(x : std_ulogic_vector) return rv_t of sometype ;
+
+        --type memory is protected
+        --    impure function read( ;
+        --    impure function write ;
+        --    impure function randomize ;
+        --end protected ;
+
+        --procedure transact(x : transaction_list ; i : view blah) ;
+
+        --procedure cast(x : sometype) return rv_t of std_logic_vector ;
+
+        -- Generic compare function, pass in whatever comparison operator
+        function compare
+          generic(
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+            function "="(l, r : t) return boolean is <> ;
+          ) parameter(op : string ; l, r : t) return boolean ;
+
     end package ;
 
     package synth is
+        --function nullrange generic(type t is (<>)) return t'range'record ;
 
-        function overlap
+        function to_signed(x : string ; base : positive := 10) return rv_t of signed ;
+
+        function to_unsigned(x : string ; base : positive := 10) return rv_t of unsigned ;
+
+        function modpow(base : unsigned ; exp : unsigned ; modulus : unsigned) return rv_t of unsigned ;
+
+        function maximum
           generic (
-            type array_t is array(type is (<>)) of type is private ;
-            function maximum(l, r : array_t'index) return boolean is <> ;
-            function minimum(l, r : array_t'index) return boolean is <> ;
-          ) parameter (
-            l, r : array_t ;
-          ) return array_t'index'range'record ;
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+          ) parameter (l, r : t) return t ;
+
+        function minimum
+          generic (
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+          ) parameter (l, r : t) return t ;
+
+        -- Convenient
+        alias "+" is "&"[string, string return string] ;
+
+        --function overlap
+        --  generic (
+        --    type array_t is array(type is (<>)) of type is private ;
+        --    function maximum(l, r : array_t'index) return boolean is <> ;
+        --    function minimum(l, r : array_t'index) return boolean is <> ;
+        --  ) parameter (
+        --    l, r : array_t ;
+        --  ) return array_t'index'range'record ;
 
     end package ;
 
@@ -28,6 +229,24 @@ end package ;
 package body util is
 
     package body sim is
+
+        function compare
+          generic (
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+            function "="(l, r : t) return boolean is <> ;
+          ) parameter(op : string ; l, r : t) return boolean is
+        begin
+            case op is
+                when "<"    => return l < r ;
+                when "<="   => return (l < r) or (l = r) ;
+                when "="    => return l = r ;
+                when "/="   => return not (l = r) ;
+                when ">="   => return not(l < r) ;
+                when ">"    => return (not(l < r)) and (not(l = r)) ;
+                when others => return false ;
+            end case ;
+        end function ;
 
         -- can create a string for any value
         impure function to_string(variable value : VALUE_MIRROR) return STRING is
@@ -151,46 +370,140 @@ package body util is
     end package body ;
 
     package body synth is
-        function overlap
-          generic (
-            type array_t is array(type is (<>)) of type is private ;
-            function maximum(l, r : array_t'index) return boolean is <> ;
-            function minimum(l, r : array_t'index) return boolean is <> ;
-          ) parameter (
-            l, r : array_t ;
-          ) return array_t'index'range'record
-        is
-            constant lrange : array_t'index'range'record := l'range'value ;
-            constant rrange : array_t'index'range'record := r'range'value ;
-            variable orange : array_t'index'range'record := l'range'value ;
+
+        --function nullrange generic(type t is (<>)) return t'range'record is
+        --    subtype rv is t range t'high to t'low ;
+        --begin
+        --    return rv'range'record ;
+        --end function ;
+
+        function to_integer(x : character) return rv_t of integer is
+            constant rv : natural := character'pos(x) - character'pos('0') when character'pos(x) < 58 else
+                                     character'pos(x) - character'pos('A') + 10 when character'pos(x) < 71 else
+                                     character'pos(x) - character'pos('a') + 10 ;
         begin
-            if lrange.direction /= rrange.direction then
-                -- Choosing to be ascending
-                orange.direction := ascending ;
-                if lrange.direction = ascending then
-                    -- L: low to high
-                    -- R: high downto low
-                    orange.left := maximum(lrange.left, rrange.right) ;
-                    orange.right := minimum(lrange.right, rrange.left) ;
-                else
-                    -- L: high downto low
-                    -- R: low to high
-                    orange.left := maximum(lrange.right, rrange.left) ;
-                    orange.right := minimum(lrange.left, rrange.right) ;
-                end if ;
-            else
-                if lrange.direction = ascending then
-                    -- low to high
-                    orange.left := maximum(lrange.left, rrange.left) ;
-                    orange.right := minimum(lrange.right, rrange.right) ;
-                else
-                    -- high downto low
-                    orange.left := minimum(lrange.left, rrange.left) ;
-                    orange.right := maximum(lrange.right, rrange.right) ;
-                end if ;
-            end if ;
-            return orange ;
+            return rv ;
         end function ;
+
+        function modpow(base : unsigned ; exp : unsigned ; modulus : unsigned) return rv_t of unsigned is
+            variable lbase : base'subtype := base ;
+            variable lexp : exp'subtype := exp ;
+            variable rv : rv_t ;
+        begin
+            if modulus = 1 then
+                return to_unsigned(0, rv'length) ;
+            end if ;
+            rv := to_unsigned(1, rv'length) ;
+            lbase := lbase mod modulus ;
+            while lexp > 0 loop
+                if lexp(0) = '1' then
+                    rv := (rv * lbase) mod modulus ;
+                end if ;
+                lexp := shift_right(lexp, 1) ;
+                lbase := (lbase * lbase) mod modulus ;
+            end loop ;
+            return rv ;
+        end function ;
+
+        function to_signed(x : string ; base : positive := 10) return rv_t of signed is
+            variable rv : rv_t := (others =>'0') ;
+            constant negate : boolean := true when x(1) = '-' else false ;
+            variable idx    : positive := 2 when negate = true else 1 ;
+        begin
+            assert base = 10 or base = 8 or base = 2 or base = 16
+                report "to_signed: Unsupported base (" & to_string(base) & ")"
+                severity failure ;
+            while idx < x'length loop
+                rv := rv * base ;
+                rv := rv + to_integer(x(idx)) ;
+            end loop ;
+            case base is
+                when  2 => assert x = to_bstring(rv) report "Conversion Error" severity warning ;
+                when  8 => assert x = to_ostring(rv) report "Conversion Error" severity warning ;
+                when 10 => assert x = to_string(rv)  report "Conversion Error" severity warning ;
+                when 16 => assert x = to_hstring(rv) report "Conversion Error" severity warning ;
+                when others => report "Invalid base" severity failure ;
+            end case ;
+            return rv when negate = false else -rv ;
+        end function ;
+
+        function to_unsigned(x : string ; base : positive := 10) return rv_t of unsigned is
+            variable rv : rv_t := (others =>'0') ;
+        begin
+            for idx in x'range loop
+                rv := rv * base ;
+                rv := rv + to_integer(x(idx)) ;
+            end loop ;
+            case base is
+                when  2 => assert x = to_bstring(rv) report "Conversion Error" severity warning ;
+                when  8 => assert x = to_ostring(rv) report "Conversion Error" severity warning ;
+                when 10 => assert x = to_string(rv)  report "Conversion Error" severity warning ;
+                when 16 => assert x = to_hstring(rv) report "Conversion Error" severity warning ;
+                when others => report "Invalid base" severity failure ;
+            end case ;
+            return rv ;
+        end function ;
+
+        function maximum
+          generic (
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+          ) parameter(l, r : t) return t
+        is
+        begin
+            return l when (r < l) else r ;
+        end function ;
+
+        function minimum
+          generic (
+            type t ;
+            function "<"(l, r : t) return boolean is <> ;
+          ) parameter(l, r : t) return t
+        is
+        begin
+            return l when (l < r) else r ;
+        end function ;
+
+        --function overlap
+        --  generic (
+        --    type array_t is array(type is (<>)) of type is private ;
+        --    function maximum(l, r : array_t'index) return boolean is <> ;
+        --    function minimum(l, r : array_t'index) return boolean is <> ;
+        --  ) parameter (
+        --    l, r : array_t ;
+        --  ) return array_t'index'range'record
+        --is
+        --    constant lrange : array_t'index'range'record := l'range'value ;
+        --    constant rrange : array_t'index'range'record := r'range'value ;
+        --    variable orange : array_t'index'range'record := l'range'value ;
+        --begin
+        --    if lrange.direction /= rrange.direction then
+        --        -- Choosing to be ascending
+        --        orange.direction := ascending ;
+        --        if lrange.direction = ascending then
+        --            -- L: low to high
+        --            -- R: high downto low
+        --            orange.left := maximum(lrange.left, rrange.right) ;
+        --            orange.right := minimum(lrange.right, rrange.left) ;
+        --        else
+        --            -- L: high downto low
+        --            -- R: low to high
+        --            orange.left := maximum(lrange.right, rrange.left) ;
+        --            orange.right := minimum(lrange.left, rrange.right) ;
+        --        end if ;
+        --    else
+        --        if lrange.direction = ascending then
+        --            -- low to high
+        --            orange.left := maximum(lrange.left, rrange.left) ;
+        --            orange.right := minimum(lrange.right, rrange.right) ;
+        --        else
+        --            -- high downto low
+        --            orange.left := minimum(lrange.left, rrange.left) ;
+        --            orange.right := maximum(lrange.right, rrange.right) ;
+        --        end if ;
+        --    end if ;
+        --    return orange ;
+        --end function ;
 
     end package body ;
 
